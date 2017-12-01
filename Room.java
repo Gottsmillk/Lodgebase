78fa3fa08d11cdbde13dbe220f1294f397bca272
Austin <austin.p.2013@gmail.com>
2017-12-01 01:24:26 -0500

    room.java

---
+++ Room.java
@@ -0,0 +1,17 @@
+package BookingSystem;
+public class Room{
+	int roomID = -1;
+	int numBeds = -1;
+	int numGuests = -1;
+	public int getRoomID(){
+		return roomID;
+	}
+	public int getNumBeds(){
+		return numBeds;
+	}
+	public int getNumGuests(){
+		return numGuests;
+	}
+
+
+}
